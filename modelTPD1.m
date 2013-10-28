a0562879d1dd90face734c361bc550704cf45587
Misha Doronin <misha@doronin.org>
2013-10-28 19:00:54 +0100

    Move odemlde to modelTPD1 inner function

--- modelTPD1.m
+++ modelTPD1.m
@@ -9,23 +9,36 @@ function [ Tode, theta, p ] = modelTPD1(T,theta0,v,E)
 
 k = 1.38e-23*6.2e18; % (J K^-1)*(eV/J) = eV K^-1 = 8.6e-5 eV K^-1
 odepar.ek1=E/k;
-
 a = 3/60; % K/s, heating rate
 odepar.nuoa=v/a;
-
 odepar.myzero=10*eps;
 
-
 vopt=odeset("NormControl","on","Stats","off");
-
 
 tic();
 ret=ode45(@odemlde,T,theta0,vopt,odepar);
 toc()
-#ret.stats
+
+if (isfield(ret,"stats"))
+	ret.stats
+endif
 Tode=ret.x;
 theta=ret.y;
 p=-arrayfun(@odemlde,Tode,theta,odepar);
 
 endfunction;
 
+#ODE for multilayer desorption, zero order, then first order
+function dthetadt = odemlde(T,theta,param)
+	nuovera=param.nuoa;
+	Ek1=param.ek1;
+	Eoverk=param.ek1;
+	myzero =param.myzero;
+	if (theta > 1)
+		dthetadt = -nuovera.*exp(-Eoverk./(T+eps));
+	elseif (theta > myzero)
+		dthetadt = -nuovera.*theta.*exp(-Eoverk./(T+eps));
+	else
+		dthetadt = 0;
+	end
+endfunction;
