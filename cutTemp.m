37d0a412279092c844573aaa68b68626abea56c8
Misha Doronin <misha@doronin.org>
2013-11-28 19:10:36 +0100

    Display the mass data even if not enough points in the range

--- cutTemp.m
+++ cutTemp.m
@@ -2,7 +2,14 @@
 function result=cutTemp(struct,cutT);
 	numpoints=length(struct.T);
 	minidx=max(max(find(struct.T<=cutT.min)),1);
-	maxidx=min(numpoints,min(find(struct.T >= cutT.max)));
+	if (isnan(minidx) || length(minidx)==0)
+		minidx=1;
+	endif;
+	maxidx=min(numpoints,min(find(struct.T >= cutT.max)))
+	
+	if (isnan(maxidx) || length(maxidx)==0);
+		maxidx=numpoints;
+	endif;
 	
 	result=cutIndex(struct,numpoints,minidx,maxidx);
 endfunction
