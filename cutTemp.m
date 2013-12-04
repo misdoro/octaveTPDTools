1da703aa3f21a9326a32f85b24736672470c5b15
Misha Doronin <misha@doronin.org>
2013-12-04 17:32:07 +0100

    Fix cutTemp to return empty structure when out of range

--- cutTemp.m
+++ cutTemp.m
@@ -5,7 +5,8 @@ function result=cutTemp(struct,cutT);
 	if (isnan(minidx) || length(minidx)==0)
 		minidx=1;
 	endif;
-	maxidx=min(numpoints,min(find(struct.T >= cutT.max)))
+	
+	maxidx=min(numpoints,min(find(struct.T >= cutT.max)));
 	
 	if (isnan(maxidx) || length(maxidx)==0);
 		maxidx=numpoints;
@@ -13,6 +14,14 @@ function result=cutTemp(struct,cutT);
 	
 	if (minidx>maxidx)
 		minidx=max(max(find(struct.T(1:maxidx)<=cutT.min)),1);
+	endif;
+	
+	if (minidx==numpoints || maxidx==1)
+		result.T=[];
+		result.t=[];
+		result.i=[];
+		result.intg=0;
+		return;
 	endif;
 	
 	result=cutIndex(struct,numpoints,minidx,maxidx);
