649a777fb142fa866af43c86ad5d15b347d0e9ec
Misha Doronin <misha@doronin.org>
2014-01-06 18:39:56 +0100

    Use lsode-employing model function, parameter to enable or disable parallel execution

--- makemodeldat.m
+++ makemodeldat.m
@@ -6,7 +6,7 @@ par.mids=[0 84];
 par.rate=15/60;
 par.np=321;
 par.minT=25;
-par.maxT=60;
+par.maxT=55;
 par.theta=0.16;
 par.thetas=0.001;
 par.v=3.7e+11;
@@ -16,9 +16,11 @@ par.monolay=2e-8;
 par.bline=1e-13;
 par.numsim=10;
 
+par.parallel=3;
 
-[f.info, f.err, f.msg]=stat("param.m")
-if (f.err>=0)
+
+[f.info, f.err, f.msg]=stat("param.m");
+if (f.err>=0);
 	printf("Loading param\n");
 	source("param.m");
 endif;
@@ -28,12 +30,12 @@ pkg load odepkg
 pkg load general
 
 function press=calcP1(tpd,parv)
-	[T,theta,p0]=modelTPD1(tpd.T,parv(2),parv(1),parv(4),tpd.rate);
+	[T,theta,p0]=modelTPDlsode(tpd.T,parv(2),parv(1),parv(4),tpd.rate);
 	press=p0*parv(3);
 endfunction;
 
 function press=calcP1a(para)
-	[T,theta,p0]=modelTPD1(para.T,para.b,para.a,para.d,para.rate);
+	[T,theta,p0]=modelTPDlsode(para.T,para.b,para.a,para.d,para.rate);
 	press=p0*para.c;
 endfunction;
 
@@ -47,24 +49,35 @@ function ptotn=calcPn(tpd,parv,par)
 	cspr=par.thetas;
 	parm(:,4)=linspace(pars(4)-espr,pars(4)+espr,numsim);
 	parm(:,2)=linspace(pars(2)-cspr,pars(2)+cspr,numsim);
-
-	for i=1:numsim
-		para(i).a=parm(i,1);
-		para(i).b=parm(i,2);
-		para(i).c=parm(i,3);
-		para(i).d=parm(i,4);
-		para(i).T=tpd.T;
-		para(i).rate=tpd.rate;
-		
-	endfor
-	tic();
-	ptotn=sum(pararrayfun(3,@calcP1a,para),2);
-	toc()
+	
+	
+	if (par.parallel<2);
+		tic();
+		ptotn=zeros(length(tpd.T),1);
+	
+		for i=1:numsim
+			ptotn+=calcP1(tpd,parm(i,:));
+		endfor
+		toc()
+	else
+	
+		tic();
+		for i=1:numsim
+			para(i).a=parm(i,1);
+			para(i).b=parm(i,2);
+			para(i).c=parm(i,3);
+			para(i).d=parm(i,4);
+			para(i).T=tpd.T;
+			para(i).rate=tpd.rate;
+		endfor
+		ptotn=sum(pararrayfun(par.parallel,@calcP1a,para),2);
+		toc()
+	endif;
 endfunction;
 
 tpd.mids=par.mids;
 tpd.rate=par.rate;
-maxt=(par.maxT-par.minT)/par.rate
+maxt=(par.maxT-par.minT)/par.rate;
 tpd.t=linspace(0,maxt,par.np)';
 tpd.T=linspace(par.minT,par.maxT,par.np)';
 
