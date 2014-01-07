57380c8e0fdabbfeef9f7440eaaf010a2a9f3d5f
Misha Doronin <misha@doronin.org>
2014-01-07 18:17:50 +0100

    Cleanups and comments

--- modelTPDlsode.m
+++ modelTPDlsode.m
@@ -1,37 +1,39 @@
-function [ Tode, theta, p ] = modelTPDlsode(T,theta0,v,E,rate=3/60)
+function [ T, theta, p ] = modelTPDlsode(T,theta0,v,E,rate=3/60)
 % Model TPD curve
 %
-% T - temperatures needed, independent variable
-% N - normalization factor, area under p(T)
-% v - frequency
-% E - energy level in electron volts
+% function [ T, theta, p ] = modelTPDlsode(T,theta0,v,E,rate=3/60)
+%
+% T - temperatures needed, array of points
+% theta0 - initial coverage
+% v - frequency (desorption prefactor)
+% E - adsorption energy in electron-volts
+% rate - desorption rate, defaults to 3K/minute
+%
+% Returns:
+% T - the temperatures array
+% theta - coverages array
+% p - desorption rates array
 
 
 k = 1.38e-23*6.24e18; % (J K^-1)*(eV/J) = eV K^-1 = 8.6e-5 eV K^-1
-odepar.ek1=E/k;
-odepar.nuoa=v/rate;
-odepar.myzero=10*eps;
+#Pre-calculate parameters
+odepar.ek1=E/k; odepar.nuoa=v/rate; odepar.myzero=eps;
 
-of=@(x,t) odemlde(x,t,odepar);
-
+of=@(x,t) odemlde(x,t,odepar); #Solve the ODE
 theta=lsode(of,theta0,T);
 
-if (rows(T)>1)
-	Tode=T;
-else
-	Tode=T';
+if (rows(theta)!=rows(T))
+	theta=theta';
 endif;
 
-p=-arrayfun(@odemlde,theta,Tode,odepar);
+#Derive desorption rates from the coverage values
+p=-arrayfun(@odemlde,theta,T,odepar);
 
 endfunction;
 
 #ODE for multilayer desorption, zero order, then first order
 function dthetadT = odemlde(theta,T,param)
-	nuovera=param.nuoa;
-	Ek1=param.ek1;
-	Eoverk=param.ek1;
-	myzero =param.myzero;
+	nuovera=param.nuoa;Eoverk=param.ek1;myzero=param.myzero;
 	if (theta > 1)
 		dthetadT = -nuovera.*exp(-Eoverk./(T+eps));
 	elseif (theta > myzero)
