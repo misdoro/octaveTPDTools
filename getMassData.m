c7d99688c6ccee8cdc1c657505f3b3076c5f4378
Misha Doronin <misha@doronin.org>
2014-01-17 19:48:28 +0100

    Fix getMassData to return correct data

--- getMassData.m
+++ getMassData.m
@@ -1,9 +1,10 @@
 #Extract from the tpd structure data about requested mass in a specified temperature range
 function result = getMassData(tpd,Trange,mass);
 	massidx=find(tpd.mids==mass);
+	zerosi=min(find(tpd.mids>0));
 	if (massidx>1)
 		result=tpd;
-		result.i=tpd.iN(:,massidx-1);
+		result.i=tpd.iN(:,massidx-zerosi+1);
 		result=cutTemp(result,Trange);
 		result.intg=trapz(result.t,result.i);
 	else
