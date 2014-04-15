e67d85d333c0d90d8823f38003c118835950331f
Misha Doronin <misha@doronin.org>
2014-04-15 15:33:21 +0200

    Make the calculatedoseintegral work with weird data

--- calculateDoseIntegral.m
+++ calculateDoseIntegral.m
@@ -2,21 +2,24 @@ function [doseint,maxidx]=calculateDoseIntegral(dosedata)
 %Calculate dose integral: extract the baseline, finish integration at 1/2(max-end)
 %Returns the dose integral and the final point of the integration
 	
-	if (!isstruct(dosedata) ||\
-		!isfield(dosedata,"t")||\
-		!isfield(dosedata,"i")||\
-		length(dosedata.i)==0||\
+	if (!isstruct(dosedata) ||
+		!isfield(dosedata,"t")||
+		!isfield(dosedata,"i")||
+		length(dosedata.i)==0 ||
 		length(dosedata.t)==0);
 		doseint=0;
 		maxidx=0;
 		return;
 	endif;
 	
+	#plot(dosedata.t,dosedata.i);
+	#input("OK?")
+	
 	dose.mini=min(dosedata.i);
 	dose.maxi=max(dosedata.i);
 	dose.fini=dosedata.i(end);
-	dose.iend=(dose.maxi+dose.fini)/2;
-	dose.intend=min(max(find(dosedata.i>dose.iend)),length(dosedata.i));
+	dose.iend=(dose.maxi+3*dose.fini)/4;
+	dose.intend=min(max(find(dosedata.i>=dose.iend)),length(dosedata.i));
 	dose.isub=dosedata.i(1:dose.intend)-dose.mini;
 	dose.tsub=dosedata.t(1:dose.intend);
 	doseint=trapz(dose.tsub,dose.isub);
