05976d27924e8199353f8bcf454e789145b21fcc
Misha Doronin <misha@doronin.org>
2014-06-13 18:56:23 +0200

    Move out plotEAds.m from displayTPD

--- displayTPD.m
+++ displayTPD.m
@@ -265,27 +265,8 @@ endif;
 
 
 ##################################################
-#Stupid Eads inversion				 #
+#Eads inversion	                          			 #
 ##################################################
-
-function result=plotEAds(mytpd,param,result);
-	tpd.a=[1e13, 1e15, 1e17];
-	source("~/octave/constants.m");
-	cov=mytpd.intg-cumtrapz(mytpd.t,mytpd.i);
-	for ai=1:length(tpd.a);
-		E=-(R_eV/Na).* mytpd.T .*log(mytpd.i ./ ((tpd.a(ai)).*cov));
-		plot(cov/param.monolayer,E,"color",mytpd.color);
-		if (mytpd.idx==1)
-			[maxE,maxEpos]=max(E);
-      txt=sprintf("< %.1e ",tpd.a(ai))
-			#txt=strcat("<",num2str(mytpd.idx),":",num2str(tpd.a(ai)));
-      cov(maxEpos)
-      maxE
-			text(cov(maxEpos),maxE,txt);
-		endif;
-	end
-endfunction
-
 if (index(param.tools,'e'))
 	param.fig.eads=++param.figindex;
 	figure(param.fig.eads);
