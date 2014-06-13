ddaf09bc7e1647eace42ced89fa22f7defbdeb35
Misha Doronin <misha@doronin.org>
2014-06-13 18:57:16 +0200

    Add some inf/nan checks to ploteads

--- plotEAds.m
+++ plotEAds.m
@@ -1,16 +1,23 @@
 function result=plotEAds(mytpd,param,result);
-	tpd.a=[1e13, 1e15, 1e17, 1e19];
+	tpd.a=[1e11,1e13, 1e15, 1e17];
 	source("~/octave/constants.m");
-	cov=mytpd.intg-cumtrapz(mytpd.t,mytpd.i);
+	cov=trapz(mytpd.t,mytpd.i)-cumtrapz(mytpd.t,mytpd.i);
 	for ai=1:length(tpd.a);
-		E=-(R_eV/Na).* mytpd.T .*log(mytpd.i ./ ((tpd.a(ai)).*cov));
-		plot(cov/param.monolayer,E,"color",mytpd.color);
+		E=-(R_eV/Na).* mytpd.T .*log(mytpd.i ./ ((tpd.a(ai)).*(cov+eps)));
+    cml=cov/param.monolayer;
+    if(isinf(E(1)))
+      maxbeg=max(find(isinf(E)))
+    else
+      maxbeg=20;
+    endif
+    
+    mini=min([find(isnan(E));length(E)-10]);
+    mini
+		plot(cml(maxbeg:mini),E(maxbeg:mini),"color",mytpd.color);
 		if (mytpd.idx==1)
-			[maxE,maxEpos]=max(E);
-			txt=strcat("<",num2str(mytpd.idx),":",num2str(tpd.a(ai)));
-			text(cov(maxEpos),maxE,txt);
+			
+			txt=sprintf("< %.1e ",tpd.a(ai));
+			text(cov(mini-1),E(mini-1),txt);
 		endif;
-    ai
-    tpd.a(ai)
 	end
 endfunction
