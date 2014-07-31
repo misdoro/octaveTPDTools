9d28a533d5c4fde9f0568af1e606fe43b57b95a7
Misha Doronin <misha@doronin.org>
2014-07-31 16:06:53 +0200

    Choose color for the energy fit

--- plotfitret.m
+++ plotfitret.m
@@ -1,8 +1,8 @@
-function ret=plotfitret(filename);
+function ret=plotfitret(filename,linecolor='blue');
 load(filename);
 ret.v=fitret(:,1);
 ret.Ea=fitret(:,2);
 ret.cov=fitret(:,3);
 ret.ssq=fitret(:,4);
-semilogx(ret.v,ret.Ea,ret.v,ret.ssq/min(ret.ssq));
+semilogx(ret.v,ret.Ea,'color',linecolor,ret.v,ret.ssq/min(ret.ssq));
 endfunction;
