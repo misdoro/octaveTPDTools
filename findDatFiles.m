e599608913d207de400aad0842b86ed873fa0e2a
Misha Doronin <misha@doronin.org>
2014-08-14 16:28:03 +0200

    Generalize to enable user-defined filename suffixes

--- findDatFiles.m
+++ findDatFiles.m
@@ -1,12 +1,12 @@
 #Return a list of .dat files from a given directory
-function result=findDatFiles(directory)
+function result=findDatFiles(directory, extension="dat")
 	files=readdir(directory);
 	len=length(files);
-
+  regex=sprintf("\\.%s$",extension);
 	result={};
 
 	for idx=1:length(files);
-		if (regexpi(files{idx},"\\.dat$")>1 && ~isdir(files{idx}))
+		if (regexpi(files{idx},regex)>1 && ~isdir(files{idx}))
 			result=[result,files{idx}];
 		endif
 	end;
