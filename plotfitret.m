dfd4d258b4f347298d96a0f48e68434a1d894c43
Misha Doronin <misha@doronin.org>
2014-09-18 19:14:10 +0200

    More explicit output

--- plotfitret.m
+++ plotfitret.m
@@ -4,5 +4,22 @@ ret.v=fitret(:,1);
 ret.Ea=fitret(:,2);
 ret.cov=fitret(:,3);
 ret.ssq=fitret(:,4);
-semilogx(ret.v,ret.Ea,'color',linecolor,ret.v,ret.ssq/min(ret.ssq));
+
+figure(1);
+hold on;
+semilogx(ret.v,ret.Ea,'color',linecolor);
+xlabel("v");
+ylabel("Ea");
+
+figure(2);
+hold on;
+semilogx(ret.v,ret.ssq/min(ret.ssq),'color',linecolor);
+xlabel("v");
+ylabel("fit rms");
+
+figure(3);
+hold on;
+semilogx(ret.v,ret.cov,'color',linecolor);
+xlabel("v");
+ylabel("fit coverage");
 endfunction;
