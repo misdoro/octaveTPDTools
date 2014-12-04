0c7536e4d4b7f8000fc91ad6e072b50f9d762044
Misha Doronin <misha@doronin.org>
2014-12-04 16:21:42 +0100

    Check borders before removing a spike

--- removeSpikes.m
+++ removeSpikes.m
@@ -3,12 +3,15 @@ function ret=removeSpikes(x,y)
   dy=y-ysm;
   stddy=std(dy)*10;
   sp=find(abs(dy)>stddy);
+  sze=length(x);
   for i=1:length(sp);
     spi=sp(i);
-    prevy=y(spi);
-    newy=mean([y(spi-1),y(spi+1)]);
-    y(spi)=newy;
-    printf("replaced a spike at %d of %.3e with %.3e\n",spi,prevy,newy);
+    if (spi<sze && spi>1)%Proceed only if spike is detected before the last element and after the first.
+      prevy=y(spi);
+      newy=mean([y(spi-1),y(spi+1)]);
+      y(spi)=newy;
+      printf("replaced a spike at %d of %.3e with %.3e\n",spi,prevy,newy);
+    endif
   end
   ret=y;
 endfunction
