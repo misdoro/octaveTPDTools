2181942ad78ea8f9a8d3f76ea59739571b7d3b59
Misha Doronin <misha@doronin.org>
2015-01-16 15:40:32 +0100

    Clean up TPD model function

--- modelTPDmc.m
+++ modelTPDmc.m
@@ -1,10 +1,11 @@
-function [p,theta]=modelTPDmc(T,thetavec,par)
-%function [theta,p]=modelTPDmc(T,thetavec,par)
-%Model TPD curve as a sum of sites with coverages thetavec,
-%par has par.E0,par.dE,par.v,par.rate attributes
+function [p,theta]=modelTPDmc(T,fitpar)
+%function [p,theta]=modelTPDmc(T,fitpar)
+%Model first order TPD curve as a sum of sites with coverages fitpar.thetas,
+%fitpar should have following attributes:
 %energies Ei=E0+i*dE,
 %prefactor v,
 %heating rate rate
+%ml monolayer integral
 
 % Returns:
 % p - desorption rates array
@@ -14,7 +15,7 @@ E0=par.E0;
 dE=par.dE;
 v=par.v;
 rate=par.rate;
-monolay=par.monolay;
+monolay=par.ml;
 
 k = 1.38e-23*6.24e18; % (J K^-1)*(eV/J) = eV K^-1 = 8.6e-5 eV K^-1
 
@@ -23,36 +24,29 @@ E=linspace(E0,E0+dE*(numpts-1),numpts);
 #Pre-calculate parameters
 odepar.ek1=E/k; odepar.nuoa=v/rate;
 
-if (rows(thetavec)!=rows(T))
-	thetavec=thetavec';
+if (rows(thetas)!=rows(T))
+	thetas=thetas';
 endif;
 
 of=@(x,t) odemlde(x,t,odepar); #Solve the ODE
-theta=lsode(of,thetavec,T);
+theta=lsode(of,thetas,T);
 
-#Derive desorption rates from the coverage values
+#Derive desorption rates from coverage values
 numT=length(T);
 p=zeros(numT,1);
-
-#This one is slow...
 for i=1:numT
-p(i)=(-sum(odemlde(theta(i,:)',T(i),odepar)));
+  #For each temperature point find desorption flow as a sum of flows defined by remaining coverages
+  p(i)=(-sum(odemlde(theta(i,:)',T(i),odepar)));
 endfor
 
+#Switch from 1/K to 1/sec.
 p=p*rate*par.monolay;
 
 endfunction;
-
 
 #ODE for first order desorption
 function dthetadT = odemlde(thetavec,T,param)
 	nuovera=param.nuoa;
   Eoverk=param.ek1;
   dthetadT = (thetavec>eps)'.*(-nuovera).*thetavec'.*exp(-Eoverk./(T+eps));
-endfunction;
-function dthetadT = odemlde1(thetavec,T,param)
-	nuovera=param.nuoa;
-  Eoverk=param.ek1;
-  Eoverk=Eoverk(param.i);
-  dthetadT = (thetavec>eps).*(-nuovera).*thetavec.*exp(-Eoverk./(T+eps));
 endfunction;
