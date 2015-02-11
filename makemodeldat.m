b551d9809861c90bff658b24b20768d6c92b243f
Misha Doronin <misha@doronin.org>
2015-02-11 16:29:07 +0100

    Use modelTPDmc model function, better param.m and input handling

--- makemodeldat.m
+++ makemodeldat.m
@@ -1,39 +1,50 @@
 #!/usr/bin/octave --persist
 #Model a TPD dat file
 
-par.mids=[84];
-
-par.rate=15/60;
-par.np=321;
-par.minT=25;
-par.maxT=55;
-par.theta=0.16;
-par.thetas=0.001;
-par.v=3.7e+11;
-par.E=0.11;
-par.Es=0.007;
-par.monolay=2e-8;
-par.bline=1e-13;
-par.numsim=10;
-par.ls=1;
-par.parallel=1;
-
-
+par.bline=0;
+par.scale=1;
+par.ml=2e-8;
 par=loadParamFile(par);
+fields={'mid','rate','npT','minT','maxT','v','E0','dE','thetas','bline'};
 
+for i=1:length(fields)
+  if (~isfield(par,fields(i)))
+    printf("Param %s is missing",fields{i});
+    printf("\n\
+    param.m fields:\n\
+    par.mid=mass\n\
+    par.rate=K/sec\n\
+    par.npT=number of temperature points\n\
+    par.minT=min temperature\n\
+    par.maxT=max temperature\n\
+    par.v=prefactor\n\
+    par.E0=initial energy\n\
+    par.dE=energy step\n\
+    par.thetas=[array of partial coverages]\n\
+    par.bline=constant baseline\n\
+    optional:\n\
+    par.noise=1 to enable noise\n\
+    par.snr=snr value\n\
+    par.constnoise=constant noise\n\
+    ");
+    exit(0);
+  endif
+endfor
+par.rate=useArgument(argv(),2,par.rate);
 pkg load odepkg
 pkg load general
 
 
-tpd.mids=par.mids;
+tpd.mids=par.mid;
 tpd.rate=par.rate;
 maxt=(par.maxT-par.minT)/par.rate;
-tpd.t=linspace(0,maxt,par.np)';
-tpd.T=linspace(par.minT,par.maxT,par.np)';
+tpd.t=linspace(0,maxt,par.npT)';
+tpd.T=linspace(par.minT,par.maxT,par.npT)';
 
-parv=[par.v,par.theta,par.monolay*par.rate,par.E];
+#parv=[par.v,par.theta,par.monolay*par.rate,par.E];
 #tpd.i=calcP1(tpd,parv)+par.bline;
-tpd.i=calcPn(tpd,parv,par)+par.bline;
+#tpd.i=calcPn(tpd,parv,par)+par.bline;
+tpd.i=modelTPDmc(tpd.T,par)+par.bline;
 if(isfield(par,"noise")&& par.noise)
   snr=1000;
   if (isfield(par,"snr"))
@@ -43,7 +54,7 @@ if(isfield(par,"noise")&& par.noise)
   if (isfield(par,"constnoise"))
     noiselvl+=par.constnoise;
   endif
-  tpd.i=tpd.i.+stdnormal_rnd(par.np,1).*noiselvl;
+  tpd.i=tpd.i.+stdnormal_rnd(par.npT,1).*noiselvl;
 endif
 tpd.iN=tpd.i;
 press.t=tpd.t+0.1;
@@ -74,13 +85,12 @@ dose.iN=dose.i;
 ##########################
 #Output data truncation and save
 if (nargin>=1);
-	filename=argv(){1};
+	filename=useArgument(argv(),1,'output.dat');
 	if (!isempty(strfind(filename,".dat" )))
 		tpd.version=20140120;
 		save("-binary",filename,"tpd","dose","press");
 	endif
 else
-	printf("Usage: modelTPD filename")
+	printf("Usage: modelTPD filename rate");
 endif
-
 exit(0);
