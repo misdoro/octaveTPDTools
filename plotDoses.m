55b792680bcc7cc650d0b9120019ff5f18f7cb5a
Misha Doronin <misha@doronin.org>
2015-05-03 21:53:32 +0200

    Make tpd/dose integral plot pretty

--- plotDoses.m
+++ plotDoses.m
@@ -1,15 +1,17 @@
 #Plot doses from struct.
 #Uses inp.doses, inp.integrals arrays
 function plotDoses(inp);
-
+inp.doses*=1e10;
+inp.integrals*=1e10;
 collage=polyfit(inp.doses,inp.integrals,1);
 points=[0;max(inp.doses)];
 plot(inp.doses,inp.integrals,'o',points,polyval(collage,points));
 
-for idx=1:length(inp.doses)
-	text(inp.doses(idx),inp.integrals(idx),strcat(" <",num2str(idx)));
+np=length(inp.doses);
+for idx=1:np
+	text(inp.doses(idx),inp.integrals(idx),sprintf(" <%d",np+1-idx));
 end
 
-xlabel("Dose integrals, A*S");
-ylabel("TPD integrals, A*S");
+xlabel("Dose integrals, ×10-10 AS");
+ylabel("TPD integrals, ×10-10 AS");
 endfunction;
