d6f52215d090b65e4eb2b703792f0e79207ec8b9
Misha Doronin <misha@doronin.org>
2015-05-17 22:16:06 +0200

    Use fminunc for coarse fit, it seems to work and be faster than fminsearch

--- fitCovScale.m
+++ fitCovScale.m
@@ -4,14 +4,15 @@ fitopts=optimset("Display","final","MaxIter",3000,"TolX",1e-5))
 printf("Optimising coverage scaling\n");
 fh=@(covsc)fitcovsc(tpd,fitpar,covsc);
 
-[covo,ssq]=fminsearch(fh,fitpar.scale,fitopts);
+[covo,ssq,info]=fminunc(fh,fitpar.scale,fitopts);
+info
 fitpar.scale=abs(covo);
 endfunction
 
 function ssq=fitcovsc(tpd,fitpar,covsc)
   fitpar.scale=abs(covsc);
   p=modelTPDmc(tpd.T,fitpar);
-	ssq=sumsq(p-tpd.i);
+	ssq=sumsq(p-tpd.i)*1e20;
   if isfield(fitpar,'debug')
     printf("SSQ %e\n",ssq);
   endif
--- fitE0.m
+++ fitE0.m
@@ -4,8 +4,8 @@ fitopts=optimset("Display","final","MaxIter",3000,"TolX",1e-5))
 printf("Optimising initial energy point\n");
 fh=@(E0)fitcovsc(tpd,fitpar,E0);
 
-[E0o,ssq]=fminsearch(fh,fitpar.E0,fitopts);
-
+[E0o,ssq,info]=fminunc(fh,fitpar.E0,fitopts);
+info
 printf("Optimized value:%f\n",abs(E0o));
 fitpar.E0=abs(E0o);
 endfunction
@@ -13,8 +13,8 @@ endfunction
 function ssq=fitcovsc(tpd,fitpar,E0)
   fitpar.E0=abs(E0);
   p=modelTPDmc(tpd.T,fitpar);
-	ssq=sumsq(p-tpd.i);
-  if isfield(fitpar,'debug')
+	ssq=sumsq(p-tpd.i)*1e20;
+  if (isfield(fitpar,'debug')&&fitpar.debug)
     printf("SSQ %e\n",ssq);
   endif
 endfunction;
